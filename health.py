8739291967ffe8ee06bd65c0a7046d35f4cd706c
Bliss Man <blissman@gmail.com>
2018-08-11 15:17:53 -0400

    add difficulty modifier

--- health.py
+++ health.py
@@ -2,7 +2,9 @@ import random
 
 health = 50
 
-potion_health = random.randint(25, 50)
+difficulty = 1
+
+potion_health = int(random.randint(25, 50) / difficulty)
 
 health = health + potion_health
 
