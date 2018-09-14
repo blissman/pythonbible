dba090bd11eb645a193e8472bd4703866d69d58a
Bliss Man <bliss.man@cbc.ca>
2018-09-14 13:57:24 -0400

    create hello_you.py, ask for inputs

---
+++ hello_you.py
@@ -0,0 +1,16 @@
+# Ask user for name
+
+name = input("What is your name?: ")
+
+# Ask user for age
+age = input("What is your age?: ")
+
+# Ask user for city
+city = input("In what city do you live?: ")
+
+# Ask user about what they enjoy
+love = input("What's an activity you enjoy?: ")
+
+# Create output text
+
+# Print output to screen
